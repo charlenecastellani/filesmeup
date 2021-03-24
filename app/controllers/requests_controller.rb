aeb20ae942ce1b5cc76b15217961391cdc770fb8
Cathy Perrot <perrot.catherine.13@gmail.com>
2021-03-24 21:51:16 +0100

    dossier a valider

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,10 +3,9 @@ class RequestsController < ApplicationController
   def index
     @requests = policy_scope(Request)
     @emetteur_requests = @requests.where(emetteur_id: current_user.id)
-    @emetteur_requests_en_cours = @emetteur_requests.where(statut = "en cours") 
+    @doc_a_valider = @emetteur_requests.joins(:documents).where("documents.etat_document = ?", "envoyé").count
     @destinataire_requests = @requests.where(destinataire_id: current_user.id)
-    @destinataire_requests_en_cours = @destinataire_requests.where(statut = "en cours") 
-  end 
+   end 
 
   def show
     @request = Request.find(params[:id])
