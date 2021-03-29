cb3484cf1807505f3856470da44ecb79e7589e6a
charlenecastellani <charlene.castellani13@gmail.com>
2021-03-29 21:00:40 +0200

    create document

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :requests do
-    resources :documents, only: [:create]
+    resources :documents, only: [:new, :create]
   end
   resources :documents, only: [:index, :update, :destroy]
 end
