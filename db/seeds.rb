be4b2b78b5725e460eab7a9509b0926324d58040
Magnan <amathmagnan@gmail.com>
2021-04-03 16:10:39 +0200

    PDF upload and images avatar test

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,28 @@ Document.destroy_all
 Request.destroy_all
 User.destroy_all
 
-file = URI.open('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80')
+file1 = URI.open('https://res.cloudinary.com/dnh448nuc/image/upload/v1617458378/DupontMoris_t5v1ji.jpg')
+file2 = URI.open('https://res.cloudinary.com/dnh448nuc/image/upload/v1617458335/martinbob_mfjq0t.jpg')
+file3 = URI.open('https://res.cloudinary.com/dnh448nuc/image/upload/v1617458751/aubertbruno_nfgluc.jpg')
+file4 = URI.open('https://res.cloudinary.com/dnh448nuc/image/upload/v1617458432/MarinJean_icsv5h.jpg')
+file5 = URI.open('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80')
 
 user1 = User.create(nom: "Dupont", prenom: "Boris", fonction: "DAF", entreprise: "Laboite", email: "bd@laboite.com", password: "orezza")
+user1.photo.attach(io: file, filename: 'DupontMoris_t5v1ji.jpg', content_type: 'image/jpg')
+user1.save
+
 user2 = User.create(nom: "Martin", prenom: "Bob", fonction: "Auditeur", entreprise: "Lecabinet", email: "bm@lecabinet.com", password: "orezza")
+user2.photo.attach(io: file, filename: 'Martin.jpg', content_type: 'image/jpg')
+user2.save
+
 user3 = User.create(nom: "Aubert", prenom: "Bruno", fonction: "Expert-comptable", entreprise: "The New Cab", email: "ba@thecab.com", password: "orezza")
+user3.photo.attach(io: file, filename: 'Aubert.jpg', content_type: 'image/jpg')
+user3.save
+
 user4 = User.create(nom: "Marin", prenom: "Jean", fonction: "Auditeur", entreprise: "AuditRocket", email: "jm@auditrocket.com", password: "orezza")
+user4.photo.attach(io: file, filename: 'Marin.jpg', content_type: 'image/jpg')
+user5.save
+
 user5 = User.new(nom: "Briche", prenom: "Victorine", fonction: "DRH", entreprise: "Laboite", email: "vb@laboite.com", password: "orezza")
 user5.photo.attach(io: file, filename: 'victorine.jpg', content_type: 'image/jpg')
 user5.save
