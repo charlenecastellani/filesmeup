bd7dbf5e34344702d18023ad5d607e6118a796ac
charlenecastellani <charlene.castellani13@gmail.com>
2021-04-14 21:28:58 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,36 +21,36 @@ file6 = URI.open('https://images.unsplash.com/photo-1613290973355-e3dd79b45de6?i
 file7 = URI.open('https://images.unsplash.com/photo-1551849569-70e81c886c27?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzkzfHxidXNpbmVzcyUyMHdvbWVufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
 file8 = URI.open('https://images.unsplash.com/photo-1562788869-4ed32648eb72?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1352&q=80')
 
-user1 = User.create(nom: "Dupont", prenom: "Boris", fonction: "DAF", entreprise: "Laboite", email: "bd@laboite.com", password: "orezza")
-user1.photo.attach(io: file1, filename: 'victorine.jpg', content_type: 'image/jpg')
+user1 = User.create(nom: "Dupont", prenom: "Benoît", fonction: "DAF", entreprise: "La boite", email: "bd@laboite.com", password: "orezza")
+user1.photo.attach(io: file1, filename: 'benoit.jpg', content_type: 'image/jpg')
 user1.save
 
-user2 = User.create(nom: "Martin", prenom: "Bob", fonction: "Auditeur", entreprise: "Lecabinet", email: "bm@lecabinet.com", password: "orezza")
-user2.photo.attach(io: file2, filename: 'bob.jpg', content_type: 'image/jpg')
+user2 = User.create(nom: "Castellani", prenom: "Charlène", fonction: "Auditrice", entreprise: "Conseils & Expertises", email: "cc@conseils-expertises.com", password: "orezza")
+user2.photo.attach(io: file2, filename: 'charlene.jpg', content_type: 'image/jpg')
 user2.save
 
 user3 = User.create(nom: "Aubert", prenom: "Bruno", fonction: "Expert-comptable", entreprise: "The New Cab", email: "ba@thecab.com", password: "orezza")
-user3.photo.attach(io: file3, filename: 'victorine.jpg', content_type: 'image/jpg')
+user3.photo.attach(io: file3, filename: 'Bruno.jpg', content_type: 'image/jpg')
 user3.save
 
-user4 = User.create(nom: "Marin", prenom: "Jean", fonction: "Auditeur", entreprise: "AuditRocket", email: "jm@auditrocket.com", password: "orezza")
-user4.photo.attach(io: file4, filename: 'victorine.jpg', content_type: 'image/jpg')
+user4 = User.create(nom: "Mazza", prenom: "Jean", fonction: "Auditeur", entreprise: "AuditRocket", email: "jm@auditrocket.com", password: "orezza")
+user4.photo.attach(io: file4, filename: 'jean.jpg', content_type: 'image/jpg')
 user4.save
 
-user5 = User.new(nom: "Briche", prenom: "Victorine", fonction: "DRH", entreprise: "Laboite", email: "vb@laboite.com", password: "orezza")
+user5 = User.new(nom: "Briche", prenom: "Victorine", fonction: "DRH", entreprise: "La boite", email: "vb@laboite.com", password: "orezza")
 user5.photo.attach(io: file5, filename: 'victorine.jpg', content_type: 'image/jpg')
 user5.save
 
-user6 = User.create(nom: "Poutoux", prenom: "Chantal", fonction: "PDG", entreprise: "Laboite", email: "cp@laboite.com", password: "orezza")
-user6.photo.attach(io: file6, filename: 'victorine.jpg', content_type: 'image/jpg')
+user6 = User.create(nom: "Poutoux", prenom: "Chantal", fonction: "PDG", entreprise: "La boite", email: "cp@laboite.com", password: "orezza")
+user6.photo.attach(io: file6, filename: 'chantal.jpg', content_type: 'image/jpg')
 user6.save
 
-user7 = User.create(nom: "Sorro", prenom: "Rose", fonction: "Chef comptable", entreprise: "Thecompany", email: "rs@thecompany.com", password: "orezza")
-user7.photo.attach(io: file7, filename: 'victorine.jpg', content_type: 'image/jpg')
+user7 = User.create(nom: "Sorro", prenom: "Rose", fonction: "Chef comptable", entreprise: "The company", email: "rs@thecompany.com", password: "orezza")
+user7.photo.attach(io: file7, filename: 'rose.jpg', content_type: 'image/jpg')
 user7.save
 
-user8 = User.create(nom: "Aniorte", prenom: "Luc", fonction: "RAF", entreprise: "BBest", email: "la@bbest.com", password: "orezza")
-user8.photo.attach(io: file8, filename: 'victorine.jpg', content_type: 'image/jpg')
+user8 = User.create(nom: "Aniorte", prenom: "Luc", fonction: "RAF", entreprise: "L'Agence Z", email: "la@lagencez.com", password: "orezza")
+user8.photo.attach(io: file8, filename: 'luc.jpg', content_type: 'image/jpg')
 user8.save
 
 audit1 = Request.create(titre: "Audit comptes 2020", date_echeance: "15/05/2021", date_creation: "20/03/2021", destinataire: user1, emetteur: user2)
@@ -60,21 +60,43 @@ audit4 = Request.create(titre: "Audit comptes 2020", date_echeance: "30/04/2021"
 audit5 = Request.create(titre: "Audit comptes 2020", date_echeance: "31/05/2021", date_creation: "27/03/2021", destinataire: user8, emetteur: user2)
 audit6 = Request.create(titre: "Revue filiales 2020", date_echeance: "15/04/2021", date_creation: "02/03/2021", destinataire: user2, emetteur: user4)
 
-document1 = Document.create(nom: "Comptes annuels 2020", description: "bilan, compte de résultat et detail des comptes", request: audit1)
-document2 = Document.create(nom: "Texte des résolutions", description: "Juridique d'approbation des comptes 2020", request: audit2)
-document3 = Document.create(nom: "Rapport de gestion", description: "Juridique d'approbation des comptes 2020", request: audit2)
+filed1 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+filed2 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+filed3 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+filed7 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+filed12 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+filed15 = URI.open("https://www.excel-pratique.com/view/fr/logiciels/img/logiciel_de_comptabilite/logiciel-compta-pratique-excel.png")
+
+document1 = Document.create(nom: "Comptes annuels 2020", description: "bilan, compte de résultat et detail des comptes", request: audit1, etat_document: "validé")
+document1.fichier.attach(io: filed1, filename: 'compta.png', content_type: 'image/png')
+document1.save
+
+document2 = Document.create(nom: "Texte des résolutions", description: "Juridique d'approbation des comptes 2020", request: audit2, etat_document: "validé")
+document2.fichier.attach(io: filed2, filename: 'compta.png', content_type: 'image/png')
+document2.save
+
+document3 = Document.create(nom: "Rapport de gestion", description: "Juridique d'approbation des comptes 2020", request: audit2, etat_document: "validé")
+document3.fichier.attach(io: filed3, filename: 'compta.png', content_type: 'image/png')
+document3.save
 document4 = Document.create(nom: "Convocation", description: "juridique d'approbation des comptes 2020", request: audit2)
 document5 = Document.create(nom: "conventions règlementées", description: "courrier d'information sur les conventions règlementées au 31/12/2020", request: audit2)
 document6 = Document.create(nom: "Etat des immobilisations au 31/12/2020", description: "Immobilisations, dotations aux amortissements et VNC", request: audit1)
-document7 = Document.create(nom: "Relevé bancaire de décembre BNP", description: "Dernier releve bancaire de l'exercice faisant apparaitre le solde au 31/12/2020", request: audit1)
+document7 = Document.create(nom: "Relevé bancaire de décembre BNP", description: "Dernier releve bancaire de l'exercice faisant apparaitre le solde au 31/12/2020", request: audit1, etat_document: "validé")
+document7.fichier.attach(io: filed7, filename: 'compta.png', content_type: 'image/png')
+document7.save
+
 document8 = Document.create(nom: "Tableau d'amortissement emprunt BNP", description: "Echeancier en cours sur le nouvel emprunt", request: audit1)
 document9 = Document.create(nom: "Contrat emprunt BNP", description: "Contrat nouvel emprunt BNP", request: audit1)
 document10 = Document.create(nom: "Inventaire au 31/12/2020", description: "Inventaire du stock au 31/12/2020 signé par le dirigeant", request: audit1)
 document11 = Document.create(nom: "Livre de paie 2020", description: "Récapitulatif de paie en cumul du 1er janvier au 31 décembre 2020", request: audit3)
-document12 = Document.create(nom: "DSN décembre 2020", description: "Dernière DSN de l'exercice", request: audit3)
+document12 = Document.create(nom: "DSN décembre 2020", description: "Dernière DSN de l'exercice", request: audit3, etat_document: "validé")
+document12.fichier.attach(io: filed12, filename: 'compta.png', content_type: 'image/png')
+document12.save
 document13 = Document.create(nom: "Etat des CP au 31/12/2020", description: "Solde détaillé des CP par salarié au 31/12/2020", request: audit3)
 document14 = Document.create(nom: "Balance générale au 31/12/2020", description: "Balance générale à jour suite modif au format excel avec comparatif n-1", request: audit4)
-document15 = Document.create(nom: "Grand-livre général au 31/12/2020", description: "Grand-livre général à jour suite modif au format excel", request: audit4)
+document15 = Document.create(nom: "Grand-livre général au 31/12/2020", description: "Grand-livre général à jour suite modif au format excel", request: audit4, etat_document: "validé")
+document15.fichier.attach(io: filed15, filename: 'compta.png', content_type: 'image/png')
+document15.save
 document16 = Document.create(nom: "comptes annuels 2020", description: "Bilan, compte de résultat et detail des comptes", request: audit5)
 document17 = Document.create(nom: "Balance générale au 31/12/2020", description: "Balance générale au format excel", request: audit5)
 document18 = Document.create(nom: "Rapport sur les comptes annuels 2019 de la société Laboite", description: "Rapport que vous avez émis pour la société Laboite, filiale de la société Thebigboite", request: audit6)
